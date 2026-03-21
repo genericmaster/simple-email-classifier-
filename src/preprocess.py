07a0b5ad4d8992a61712ff389319748d0dcc6828
genericmaster <rakhivhanithendo@gmail.com>
2026-03-21 02:59:32 +0200

    third

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,32 +4,43 @@ import re
 import logging
 from src.utils.dataset_loader import uncleaned
 
-
-#removing uneccesary features
-uncleaned= uncleaned[['Email Address','Email Content','Category']]
-
 # dealing with null values
-def Nullvalues(df:pd.DataFrame):
-     for column in df.columns:
-          if df[column].isnull().sum()==0:
-               continue
-          else:
-               
-               df= df[df.column.dropna()]
-     return df
-
-Nullvalues(uncleaned)
+def nullvalues(df:pd.DataFrame):
+    
+    null_ratio = df.isna().mean()
+    if (null_ratio< 0.05).any():
+            df= df.dropna().reset_index(drop=True)
+    for column in df.columns:      
+          if (df[column].isna().sum()/len(df[column]))>=0.8:
+            df = df.dropna(columns=[column])
+    return df
 
 #dealing with duplicates
 def duplicates(df:pd.DataFrame):
      dupe_count = df.duplicated().sum()
-     dupe_ratio = dupe_count / len(df)
      if (dupe_count==0):
           pass
-     elif dupe_ratio<=0.05:
-          df = df.drop_duplicates().reset_index(drop=True)   
-     elif dupe_ratio>0.05:
-          logging.critical("too many duplicates")
+     elif dupe_count>0:
+          df = df.drop_duplicates().reset_index(drop=True) 
+     return df  
+
+#making sure all strings are in lower
+def lowercase(df:pd.DataFrame):
+    for column in df.columns:
+         df[column]=df[column].astype(str).str.lower().astype("string")
+    return df
+         
+#removing whitespace
+def whitespace(sentence):
+     new  =re.sub(r'\s+', ' ', str(sentence)).strip()
+     return new
+
+def removewhitespace(df:pd.DataFrame):
+     for column in df.columns:
+     
+      df[column]= df[column].apply( lambda x: whitespace(x))
      return df
 
+#removing uneccesary features
+#uncleaned= uncleaned[['Email Address','Email Content','Category']]
 
